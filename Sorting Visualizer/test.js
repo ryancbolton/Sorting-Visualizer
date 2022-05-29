f691dc08a428e639b9481d130ab17621fca1bf66
Ryan Bolton <rb1813@msstate.edu>
2022-05-28 23:17:54 -0500

    Made some progress
    I've got the draw function working to remove the graph after each call to it is made. Bubble sort is working in tandem with this to draw the sorted graph. Just have to now figure out the original problem of not being able to show each step of the sorting. I'm thinking using setInterval or chunks might be the answer

--- Sorting Visualizer/test.js
+++ Sorting Visualizer/test.js
@@ -26,6 +26,8 @@ const DUMMY_DATA = [
     { id: 'd25', value: 11}
 ];
 
+const steps = [];
+
 console.log(DUMMY_DATA)
 
 var xScale = d3
@@ -41,60 +43,93 @@ const yScale = d3
 const container = d3.select('svg')
     .classed('container', true);
 
+// var l = 24; //Scaling
+
+// Draw function to graph the data after any changes
+function draw(arr) {
+    // Removes any existing bars (rects) before each redraw
+    container.selectAll('rect').remove()
+
+    container
+    .selectAll('.bar')
+    .data(arr)
+    .enter().append("rect")
+    .transition()
+    .duration(800)
+    .attr('width', xScale.bandwidth())
+    .attr('height', (data) => 600 - yScale(data.value))
+    .attr('x', data => xScale(data.id))
+    .attr('y', data => yScale(data.value))
+    .attr("fill", "#720570");
+};
+
+//Find index of specific object using findIndex method. 
+objIndex = DUMMY_DATA.findIndex((obj => obj.id));
+
+//Update object's value properties with random values.
+DUMMY_DATA.forEach(obj => {
+    for (var i = 0; i < DUMMY_DATA.length; i++) {
+        DUMMY_DATA[i].value = Math.floor(Math.random() * 16);
+    }
+});
+
+// Calls the draw function to draw the initial unsorted graph
+draw(DUMMY_DATA);
+
 /////////////////////////////////////////////   Bubble sort function    ///////////////////////////////////////////////////////
 function bubbleSort() {
     var vals = []; //initializes array for slider array values
+    var ids = []; //initializes array for slider array ids
+    var bubblearr = []; //empty final array
 
     //Adds the values from the DUMMY_DATA array to the empty 'vals' array
     for (var i = 0; i < DUMMY_DATA.length; i++) {
         vals.push(DUMMY_DATA[i].value);
     }
 
-    //Find index of specific object using findIndex method. 
-    objIndex = DUMMY_DATA.findIndex((obj => obj.id));
-
-    //Update object's value properties.
-    DUMMY_DATA.forEach(obj => {
-        for (var i = 0; i < DUMMY_DATA.length; i++) {
-            DUMMY_DATA[i].value = Math.floor(Math.random() * 16);
-        }
-    });
+    //Adds the ids from the SLIDER_DATA array to the empty 'ids' array
+    for (var i = 0; i < DUMMY_DATA.length; i++) {
+        ids.push(DUMMY_DATA[i].id);
+    }
 
     //Bubble sort function (copied from https://www.geeksforgeeks.org/bubble-sort-algorithms-by-using-javascript/)
     for(var i = 0; i < vals.length; i++){
         // bubblearr[i] = {id: ids[i], value: vals[i]}
+        // console.log("First for loop ", DUMMY_DATA);
      
         // Last i elements are already in place  
         for(var j = 0; j < (vals.length - i -1 ); j++){
+            // console.log("Second for loop ", DUMMY_DATA);
             
             // Checking if the item at present iteration is greater than the next iteration
             if(DUMMY_DATA[j].value > DUMMY_DATA[j+1].value){    //Was vals[j] and vals[j+1]
+                // console.log("If statement ", DUMMY_DATA);
+
 
                 ///////////////// Bubblearr version  ///////////////////////
                 // If the condition is true then swap them
-                var temp = DUMMY_DATA[j].value
+                var temp = vals[j]
+                vals[j] = vals[j + 1]
+                vals[j+1] = temp
+
+                // ///////////////// Static array version  ///////////////////////
+                // // If the condition is true then swap them
+                var temp2 = DUMMY_DATA[j].value
                 DUMMY_DATA[j].value = DUMMY_DATA[j + 1].value
-                DUMMY_DATA[j+1].value = temp
+                DUMMY_DATA[j+1].value = temp2
+
+                // Calls the draw function to draw the sorted graph of data
+                // console.log(DUMMY_DATA);
+                // draw(DUMMY_DATA);
+                console.log(vals);
+                steps.push(DUMMY_DATA);
             }
         }
+        // console.log(DUMMY_DATA);
+        // draw(DUMMY_DATA);
     }
-    // Calls the draw function to graph the data
-    draw(DUMMY_DATA);
 }
 
-// Draw funciton to graph the data after any changes
-function draw(arr) {
-    container
-    .selectAll('.bar')
-    .data(arr)
-    .enter().append("rect")
-    .attr('width', xScale.bandwidth())
-    .attr('height', (data) => 600 - yScale(data.value))
-    .attr('x', data => xScale(data.id))
-    .attr('y', data => yScale(data.value))
-    .attr("fill", "#720570");
-
-    container
-    .exit()
-    .remove()
+for (var i = 0; i < steps.length; i++) {
+    setInterval(draw(steps[i]), 1000);
 }
